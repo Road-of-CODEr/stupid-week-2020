edf2fc50a4841251b38b96fa7f329621ad814189
kerochuu <kerochuu@naver.com>
2020-10-18 22:38:45 +0900

    feat: Complete 3rd week of Oct

---
+++ 2020/10/week3/kerochuu/2로몇번나누어질까.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+//  @author : blog.naver.com/kerochuu 
+//  @date : 2020. 10. 18.                   
+
+public class 몇번나누어떨어질까 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		Scanner input = new Scanner(System.in);
+		System.out.println(-calc(input.nextLong() - 1) + calc(input.nextLong()));
+	}
+
+	private static long calc(long n) {
+		long result = n;
+		for (long l = 2; l <= n; l <<= 1) {
+			result += (n / l) * (l >> 1);
+		}
+		return result;
+	}
+
+	private static int stoi(String input) {
+		return Integer.parseInt(input);
+	}
+}
